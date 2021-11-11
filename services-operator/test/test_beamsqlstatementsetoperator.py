7112e3532c2ab166a9adb693c46d9d03b935bf74
Marcel <wagmarcel@web.de>
2021-11-11 18:03:05 +0100

    Add updating of beamsqlstatementsets CR with and without savepoints
    - Update of bsqls CR is processed by operator
    - If updateStrategy is selected to be None, update just cancels the old job and redeploys
    - If updateStrategy is "savepoint" it will first trigger a savepoint, then cancel and
      restart with savepoint
    - Unit tests for update added
    - In general, operator is triggering no longer when connection to flink jobmanager
      is lost
    - Redeployment is only triggerd from the NOT_FOUND state
    
    See #35
    Closes #35
    
    Signed-off-by: Marcel <wagmarcel@web.de>

--- services-operator/test/test_beamsqlstatementsetoperator.py
+++ services-operator/test/test_beamsqlstatementsetoperator.py
@@ -49,7 +49,7 @@ class TestInit(TestCase):
         self.assertIsNone(patch.status['job_id'])
 
 
-class TestUpdates(TestCase):
+class TestMonitoring(TestCase):
     def create_ddl_from_beamsqltables(beamsqltable, logger):
         return "DDL;"
 
@@ -63,8 +63,8 @@ class TestUpdates(TestCase):
     def submit_statementset_failed(statementset, logger):
         raise target.DeploymentFailedException("Mock submission failed")
 
-    def update_status_unknown(body, patch, logger):
-        patch.status["state"] = "UNKNOWN"
+    def update_status_not_found(body, patch, logger):
+        patch.status["state"] = "NOT_FOUND"
 
     @patch('beamsqlstatementsetoperator.tables_and_views.create_ddl_from_beamsqltables',
            create_ddl_from_beamsqltables)
@@ -90,7 +90,7 @@ class TestUpdates(TestCase):
         patch.status = {}
 
         beamsqltables = {("namespace", "table"): ({}, {})}
-        target.updates(beamsqltables, None, patch,  Logger(),
+        target.monitor(beamsqltables, None, patch,  Logger(),
                        body, body["spec"], body["status"])
         self.assertEqual(patch.status['state'], "DEPLOYING")
         self.assertEqual(patch.status['job_id'], "job_id")
@@ -120,7 +120,7 @@ class TestUpdates(TestCase):
 
         beamsqltables = {("namespace", "table"): ({}, {})}
         try:
-            target.updates(beamsqltables, None, patch,  Logger(),
+            target.monitor(beamsqltables, None, patch,  Logger(),
                            body, body["spec"], body["status"])
         except kopf.TemporaryError:
             pass
@@ -152,7 +152,7 @@ class TestUpdates(TestCase):
 
         beamsqltables = {}
         with self.assertRaises(kopf.TemporaryError) as cm:
-            target.updates(beamsqltables, None, patch, Logger(),
+            target.monitor(beamsqltables, None, patch, Logger(),
                            body, body["spec"], body["status"])
             self.assertTrue(str(cm.exception).startswith(
                 "Table DDLs could not be created for namespace/name."))
@@ -161,7 +161,7 @@ class TestUpdates(TestCase):
            create_ddl_from_beamsqltables)
     @patch('beamsqlstatementsetoperator.deploy_statementset',
            submit_statementset_failed)
-    @patch('beamsqlstatementsetoperator.refresh_state', update_status_unknown)
+    @patch('beamsqlstatementsetoperator.refresh_state', update_status_not_found)
     def test_update_handle_unknown(self):
         body = {
             "metadata": {
@@ -183,7 +183,7 @@ class TestUpdates(TestCase):
 
         beamsqltables = {}
 
-        target.updates(beamsqltables, None, patch, Logger(),
+        target.monitor(beamsqltables, None, patch, Logger(),
                        body, body["spec"], body["status"])
         self.assertEqual(patch.status["state"], "INITIALIZED")
         self.assertIsNone(patch.status["job_id"])
@@ -200,7 +200,7 @@ class TestDeletion(TestCase):
         pass
 
     def cancel_job_error(logger, job_id):
-        raise Exception("Could not cancel job")
+        raise kopf.TemporaryError("Could not cancel job")
 
     @patch('kopf.info', kopf_info)
     def test_canceled_delete(self):
@@ -321,6 +321,209 @@ class TestDeletion(TestCase):
             self.assertTrue(str(cm.exception).startswith("Error trying"))
         self.assertNotEqual(patch.status["state"], "CANCELING")
 
+job_canceled = False
+
+class TestUpdate(TestCase):
+    def cancel_job(logger, job_id):
+        global job_canceled
+        job_canceled = True
+        pass
+    def get_job_status(logger, job_id):
+        return {
+            "state": "RUNNING"
+        }
+    
+    def get_job_status_not_running(logger, job_id):
+        return {
+            "state": "UNKNOWN"
+        }
+
+    def cancel_job_and_get_state(logger, body, patch):
+        pass
+
+    
+    def stop_job(logger, job_id, savepoint_dir):
+        return "savepoint_id"
+
+
+    def get_savepoint_state_successful(logger, job_id, savepoint_id):
+        return {
+            "status": "SUCCESSFUL",
+            "location": "location"
+        }
+
+
+    def get_savepoint_state_in_progress(logger, job_id, savepoint_id):
+        return {
+            "status": "IN_PROGRESS",
+            "location": "location"
+        }
+
+
+    def get_savepoint_state_not_found(logger, job_id, savepoint_id):
+        return {
+            "status": "NOT_FOUND",
+            "location": "location"
+        }
+
+    def add_message(logger, body, patch, reason, mtype):
+        pass
+
+    @patch('kopf.info', kopf_info)
+    @patch('flink_util.cancel_job', cancel_job)
+    @patch('flink_util.get_job_status', get_job_status)
+    def test_cancel_job_and_get_state_running(self):
+        global job_canceled
+        body = {           
+            "status": {
+                "job_id": "job_id"
+            } 
+        }
+        job_canceled = False
+        job_state = target.cancel_job_and_get_state(Logger(), body, None)
+        self.assertEqual("RUNNING", job_state)
+        self.assertEqual(True, job_canceled)
+
+
+    @patch('kopf.info', kopf_info)
+    @patch('flink_util.cancel_job', cancel_job)
+    @patch('flink_util.get_job_status', get_job_status_not_running)
+    def test_cancel_job_and_get_state_running(self):
+        global job_canceled
+        body = {           
+            "status": {
+                "job_id": "job_id"
+            } 
+        }
+        job_canceled = False
+        job_state = target.cancel_job_and_get_state(Logger(), body, None)
+        self.assertEqual("UNKNOWN", job_state)
+        self.assertEqual(False, job_canceled)
+
+
+    @patch('kopf.info', kopf_info)
+    @patch('beamsqlstatementsetoperator.cancel_job_and_get_state', cancel_job_and_get_state)
+    def test_update_no_savepoint(self):
+        body = {
+            "metadata": {
+                "name": "name",
+                "namespace": "namespace"
+            },
+            "spec": {
+            },
+            "status": {
+                "job_id": "job_id",
+                "state": "RUNNING",
+            } 
+        }
+        patch = Bunch()
+        patch.status = {}
+        target.update(body, body["spec"], patch, Logger())
+        self.assertEqual(patch.status["state"], "UPDATING")
+        self.assertIsNone(patch.status["savepoint_id"])
+        self.assertIsNone(patch.status["location"])
+
+
+    @patch('kopf.info', kopf_info)
+    @patch('beamsqlstatementsetoperator.cancel_job_and_get_state', cancel_job_and_get_state)
+    def test_update_none_savepoint(self):
+        body = {
+            "metadata": {
+                "name": "name",
+                "namespace": "namespace"
+            },
+            "spec": {
+                "updateStrategy": None
+            },
+            "status": {
+                "job_id": "job_id",
+                "state": "RUNNING",
+            } 
+        }
+        patch = Bunch()
+        patch.status = {}
+        target.update(body, body["spec"], patch, Logger())
+        self.assertEqual(patch.status["state"], "UPDATING")
+        self.assertIsNone(patch.status["savepoint_id"])
+        self.assertIsNone(patch.status["location"])
+
+
+    @patch('kopf.info', kopf_info)
+    @patch('flink_util.stop_job', stop_job)
+    @patch('flink_util.get_savepoint_state', get_savepoint_state_successful)
+    @patch('beamsqlstatementsetoperator.add_message', add_message)
+    def test_update_savepoint_successful(self):
+        body = {
+            "metadata": {
+                "name": "name",
+                "namespace": "namespace"
+            },
+            "spec": {
+                "updateStrategy": "savepoint"
+            },
+            "status": {
+                "job_id": "job_id",
+                "state": "RUNNING",
+            } 
+        }
+        patch = Bunch()
+        patch.status = {}
+        target.update(body, body["spec"], patch, Logger())
+        self.assertEqual(patch.status["savepoint_id"], "savepoint_id")
+        self.assertEqual(patch.status["state"], "UPDATING")
+        self.assertEqual(patch.status["location"], "location")
+
+
+    @patch('kopf.info', kopf_info)
+    @patch('flink_util.stop_job', stop_job)
+    @patch('flink_util.get_savepoint_state', get_savepoint_state_in_progress)
+    @patch('beamsqlstatementsetoperator.add_message', add_message)
+    def test_update_savepoint_in_progress(self):
+        body = {
+            "metadata": {
+                "name": "name",
+                "namespace": "namespace"
+            },
+            "spec": {
+                "updateStrategy": "savepoint"
+            },
+            "status": {
+                "job_id": "job_id",
+                "state": "RUNNING",
+            } 
+        }
+        patch = Bunch()
+        patch.status = {}
+        with self.assertRaises(kopf.TemporaryError) as cm:
+            target.update(body, body["spec"], patch, Logger())
+        self.assertEqual(patch.status["savepoint_id"], "savepoint_id")
+        self.assertEqual(patch.status["state"], "SAVEPOINTING")
+
+
+    @patch('kopf.info', kopf_info)
+    @patch('flink_util.stop_job', stop_job)
+    @patch('flink_util.get_savepoint_state', get_savepoint_state_not_found)
+    @patch('beamsqlstatementsetoperator.add_message', add_message)
+    def test_update_savepoint_not_found(self):
+        body = {
+            "metadata": {
+                "name": "name",
+                "namespace": "namespace"
+            },
+            "spec": {
+                "updateStrategy": "savepoint"
+            },
+            "status": {
+                "job_id": "job_id",
+                "state": "RUNNING",
+            } 
+        }
+        patch = Bunch()
+        patch.status = {}
+        target.update(body, body["spec"], patch, Logger())
+        self.assertIsNone(patch.status["savepoint_id"])
+        self.assertEqual(patch.status["state"], "RUNNING")
+        self.assertIsNone(patch.status["location"])
 
 if __name__ == '__main__':
     unittest.main()
